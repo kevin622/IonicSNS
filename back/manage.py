1203964512fd2e99e3c4071bce17ad0c0abb8a9b
duru <kevin622@yonsei.ac.kr>
2022-05-26 17:57:32 +0900

    added secrets.json

--- back/manage.py
+++ back/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sns_ionic.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cns.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
